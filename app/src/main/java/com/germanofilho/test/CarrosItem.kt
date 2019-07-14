8246853f8dbf0ac511e9f29b92b3369fc8a7ff44
Carlos Henrique <chsl200293@gmail.com>
2019-07-14 19:44:50 -0300

    realizado as questões do desafio

--- app/src/main/java/com/germanofilho/test/CarrosItem.kt
+++ app/src/main/java/com/germanofilho/test/CarrosItem.kt
@@ -1,7 +1,7 @@
 package com.germanofilho.test
 
 data class CarrosItem(
-	val pecas: List<PecasItem?>? = null,
-	val nome: String? = null,
-	val id: Int? = null
+		val pecas: List<PecasItem?>? = null,
+		var nome: String? = null,
+		val id: Int? = null
 )
--- app/src/main/java/com/germanofilho/test/MainActivity.kt
+++ app/src/main/java/com/germanofilho/test/MainActivity.kt
@@ -2,15 +2,15 @@ package com.germanofilho.test
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.activity_main.*
-import java.io.IOException
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var montadoraResponse : Montadoras
+    var contadorCarros : Int = 0
+    var contadorPecas : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +20,47 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         montadoraResponse = Gson().fromJson(getJsonFromAssets(), Montadoras::class.java/*TODO Change object*/)
-        val teste = montadoraResponse
+        if (montadoraResponse.montadoras != null && montadoraResponse.montadoras!!.isNotEmpty()) {
+            montadoraResponse.montadoras?.let {
+                exibirQuantidadeDeCarrosEPecas(ordenarListaDeMontadoras(it))
+            }
+        }
+    }
+
+    private fun ordenarListaDeMontadoras(montadoraList: List<MontadorasItem?>) : List<MontadorasItem?> {
+        val listaOrdenada = montadoraList.toMutableList()
+        for (montadora : MontadorasItem? in montadoraList) {
+            if (montadora?.id == 3) {
+                listaOrdenada.remove(montadora)
+                listaOrdenada.add(montadora)
+            }
+        }
+        return listaOrdenada
+    }
+
+    private fun exibirQuantidadeDeCarrosEPecas(montadoraList: List<MontadorasItem?>) {
+
+        val montadoraItemList: MutableList<MontadorasItem?> = ArrayList()
+        lateinit var carroItemList: MutableList<CarrosItem?>
+
+        for (montadoraItem : MontadorasItem? in montadoraList) {
+            montadoraItemList.add(montadoraItem)
+            if (montadoraItem?.carros != null){
+                contadorCarros = montadoraItem.carros.size
+                Log.i("NUMERO DE CARROS", "A montadora ${montadoraItem.nome} tem $contadorCarros carros")
+                carroItemList = montadoraItem.carros.toMutableList()
+                for (carro: CarrosItem? in carroItemList) {
+                    if (carro?.nome == null) carro?.nome = "com nome desconhecido"
+                    contadorPecas = if (carro?.pecas != null) carro.pecas.size else 0
+                    Log.i("NUMERO DE PEÇAS", "O carro ${carro?.nome} tem $contadorPecas peças")
+                }
+            }else{
+                contadorCarros = 0
+                contadorPecas = 0
+                val mensagemSemCarros = "A ${montadoraItem?.nome} tem $contadorPecas peças por ter $contadorCarros carros"
+                Log.i("SEM CARROS", mensagemSemCarros)
+            }
+        }
     }
 
     private fun getJsonFromAssets(): String {
@@ -28,10 +68,6 @@ class MainActivity : AppCompatActivity() {
             it.readText()
         }
     }
-
-
-
-
 }
 
 
